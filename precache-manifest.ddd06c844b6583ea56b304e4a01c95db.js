085dcbe121a824c0da344f382d12d8d145630f23
malisumit86 <malisumit8616@gmail.com>
2020-08-22 18:07:57 +0530

    Updates

---
+++ precache-manifest.ddd06c844b6583ea56b304e4a01c95db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "801bccbc6f01f1db2628af6276f5e6a9",
+    "url": "/whats-app-clone_/index.html"
+  },
+  {
+    "revision": "e922d9d3d72e1d856dd6",
+    "url": "/whats-app-clone_/static/css/main.9b2bcdfc.chunk.css"
+  },
+  {
+    "revision": "400e2353d89e53111d64",
+    "url": "/whats-app-clone_/static/js/2.bb0c4c50.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/whats-app-clone_/static/js/2.bb0c4c50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e922d9d3d72e1d856dd6",
+    "url": "/whats-app-clone_/static/js/main.a96bcb13.chunk.js"
+  },
+  {
+    "revision": "53df81d0beaa3681bd21",
+    "url": "/whats-app-clone_/static/js/runtime-main.c595e7f7.js"
+  }
+]);
